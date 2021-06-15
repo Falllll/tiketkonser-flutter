dae3db4a7e78b98849e11ae4cee415c2254b4065
Falllll <falldyshinoda@gmail.com>
2021-06-15 08:30:40 +0700

    upload project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:ticketkonserflutter/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'SF Pro Display'),
+      title: 'Ticket Konser',
+      home: HomePage(),
+    );
+  }
+}
